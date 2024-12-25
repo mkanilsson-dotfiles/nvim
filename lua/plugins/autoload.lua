efa5bbb1c81093f8da2dc48c683a031ea4ad0a4f
Marcus Nilsson <marcus.nilsson@genarp.com>
2024-12-25 20:49:36 +0100

    Make autoload actually autoload

--- lua/plugins/autoload.lua
+++ lua/plugins/autoload.lua
@@ -1,2 +1,10 @@
-require("plugins.tt")
-require("plugins.format")
+local path = vim.fn.stdpath("config") .. "/lua/plugins"
+
+local handle = vim.loop.fs_scandir(path)
+while true do
+    local name, type = vim.loop.fs_scandir_next(handle)
+    if not name then break end
+    if type == 'file' and name ~= "autoload.lua" then
+        require("plugins." .. name:gsub("%.lua", ""))
+    end
+end
