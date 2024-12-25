1696f50e34ec6f26209492951a57168f1bdbe331
Marcus Nilsson <marcus.nilsson@genarp.com>
2024-12-25 16:47:58 +0100

    Hide tabline

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ vim.opt.shiftwidth = 4
 vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.showtabline = 0
 
 vim.keymap.set("n", "<space>ls", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
