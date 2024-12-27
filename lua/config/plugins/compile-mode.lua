f1e4f201af6a92b6344a45a806acb26530e41306
Marcus Nilsson <marcus.nilsson@genarp.com>
2024-12-27 14:09:01 +0100

    Add compile-mode

---
+++ lua/config/plugins/compile-mode.lua
@@ -0,0 +1,14 @@
+return {
+    "ej-shafran/compile-mode.nvim",
+    tag = "v5.4",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "m00qek/baleia.nvim", tag = "v1.3.0" },
+    },
+    config = function()
+        ---@type CompileModeOpts
+        vim.g.compile_mode = {
+            baleia_setup = true,
+        }
+    end
+}
