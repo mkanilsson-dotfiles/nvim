55cacf04b4c0b4792307ab512ce0556d68d28f7f
Marcus Nilsson <marcus.nilsson@genarp.com>
2024-12-27 15:18:54 +0100

    Add more patterns for project detection

--- lua/config/plugins/project.lua
+++ lua/config/plugins/project.lua
@@ -2,7 +2,22 @@ return {
     {
         "ahmedkhalf/project.nvim",
         config = function()
-            require("project_nvim").setup {}
+            require("project_nvim").setup {
+                patterns = {
+                    ".git",
+                    "_darcs",
+                    ".hg",
+                    ".bzr",
+                    ".svn",
+                    "Makefile",
+                    "package.json",
+                    "go.mod",
+                    "Cargo.toml",
+                    "*.opam",
+                    "dune-project",
+                    "*.sln"
+                },
+            }
             require('telescope').load_extension('projects')
 
             vim.keymap.set("n", "<space>pp", require("telescope").extensions.projects.projects, {
