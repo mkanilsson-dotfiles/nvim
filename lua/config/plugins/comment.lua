c22da2983901df96a5f30f559861e6c3fc7c330d
Marcus Nilsson <marcus.nilsson@genarp.com>
2024-12-27 14:08:08 +0100

    Fix keybinding for comment

--- lua/config/plugins/comment.lua
+++ lua/config/plugins/comment.lua
@@ -3,6 +3,10 @@ return {
         "numToStr/Comment.nvim",
         config = function()
             require("Comment").setup {
+                toggler = {
+                    line = "<C-'>",
+                    block = 'gbc',
+                },
                 opleader = {
                     line = "<C-'>",
                     block = "gbc"
