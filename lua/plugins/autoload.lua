ef291017f38a638270dc8a3701fdf9d611b1f532
Marcus Nilsson <marcus.nilsson@genarp.com>
2025-01-10 13:39:45 +0100

    Make autoload call `setup`

--- lua/plugins/autoload.lua
+++ lua/plugins/autoload.lua
@@ -2,9 +2,11 @@ local path = vim.fn.stdpath("config") .. "/lua/plugins"
 
 local handle = vim.loop.fs_scandir(path)
 while true do
-    local name, type = vim.loop.fs_scandir_next(handle)
+    local name, entity_type = vim.loop.fs_scandir_next(handle)
     if not name then break end
-    if type == 'file' and name ~= "autoload.lua" then
-        require("plugins." .. name:gsub("%.lua", ""))
+    if entity_type == 'file' and name ~= "autoload.lua" then
+        local path = "plugins." .. name:gsub("%.lua", "")
+        local plugin = require(path)
+        if type(plugin) == "table" and plugin.setup then plugin.setup() end
     end
 end
