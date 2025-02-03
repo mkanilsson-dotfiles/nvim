686a303bdc7c08eaa6185f56e1a1b19a092903ac
Marcus Nilsson <marcus.nilsson@genarp.com>
2025-02-03 09:23:57 +0100

    Increase max filesize for treesitter

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -37,7 +37,7 @@ return {
                 highlight = {
                     enable = true,
                     disable = function(lang, buf)
-                        local max_filesize = 100 * 1024 -- 100 KB
+                        local max_filesize = 500 * 1024 -- 500 KB
                         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
                         if ok and stats and stats.size > max_filesize then
                             return true
@@ -45,6 +45,10 @@ return {
                     end,
                     additional_vim_regex_highlighting = true,
                 },
+                playground = {
+                    enable = true,
+                    updatetime = 25
+                }
             }
         end
     }
