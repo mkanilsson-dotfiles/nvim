0b2896825d88ea71cd688177f9f8310f00a1d25f
Marcus Nilsson <marcus.nilsson@genarp.com>
2026-05-28 16:22:14 +0200

    Allow unsecure http requests

--- lua/config/plugins/http.lua
+++ lua/config/plugins/http.lua
@@ -1,5 +1,13 @@
 return {
     {
-        "rest-nvim/rest.nvim"
+        "rest-nvim/rest.nvim",
+        config = function()
+            -- @type rest.Opts
+            vim.g.rest_nvim = {
+                request = {
+                    skip_ssl_verification = true,
+                }
+            }
+        end
     }
 }
